9757bca6fb303bf536fc03840da38e1a6f3e665d
Anderson991288 <68816726+Anderson991288@users.noreply.github.com>
2023-02-26 18:35:18 +0800

    Update PC.v

--- code/PC.v
+++ code/PC.v
@@ -1,28 +1,3 @@
-/*
- * Ask me anything: via repo/issue, or e-mail: vencifreeman16@sjtu.edu.cn.
- * Author: @VenciFreeman (GitHub), copyright 2019.
- * School: Shanghai Jiao Tong University.
-
- * Description:
- * This file controls signal PC, it needs to send PC+4 and instruction to id.v
- * through inst_mem.v. For 5 stage stall, it need to keep PC.
-
- * Details:
- * - PC + 4 per cycle;
- * - PC value send to inst_mem as the address of instruction, then send the
- *   instruction to decode module;
- * - If current instruction is beq, blt or jal, update PC immediately.
-
- * History:
- * - 19/12/05: Create this file, add PC module;
- * - 19/12/23: Add UpdatePC module;
- * - 19/12/26: Edit module and change input/output;
- * - 19/12/18: Finished!
-
- * Notes:
- * - Take care that the module name can't be "if". Maybe it's a keep word.
- */
-
 module PC(
 
 	input	wire 		clk,
