02c90e4278afb81dd1ea9186c7d47ed70abe0df9
Anderson991288 <68816726+Anderson991288@users.noreply.github.com>
2023-02-26 18:35:31 +0800

    Update Registers.v

--- code/Registers.v
+++ code/Registers.v
@@ -1,35 +1,4 @@
-/*
- * Ask me anything: via repo/issue, or e-mail: vencifreeman16@sjtu.edu.cn.
- * Author: @VenciFreeman (GitHub), copyright 2019.
- * School: Shanghai Jiao Tong University.
-
- * Description:
- * This file is the registers of RISC-V CPU. 
-
- * Details:
- * - When we need to read readReg1, readReg2, read data from register file
- *   according to the address in readReg1, readReg2;
- * - When we need to write rd, write data into register file according to the
- *   address in rd;
- * - The operation of writing data to the register file uses sequential logic, and
- *   the operation of reading data from register file using combinational logic;
- * - X0 is a constant zero register in RISC-V. When the target register rd is X0,
- *   the data won't actually be written to X0;
- * - If the read and write register signals are valid at the same time, and if the
- *   read address is the same as the write address, then the data which need to
- *   write can be directly output as read data to achieve data forwarding.
-
- * History:
- * - 19/12/14: Create this file;
- * - 19/12/23: Modify the format;
- * - 19/12/26: Edit module¡F
- * - 19/12/28: Finished!
-
- * Notes:
- * - Single cycle donesn't need data forward.
- */
-
- module Registers(
+module Registers(
 
     input   wire        clk,
     input   wire        rst,
